a759d79d6865179f5885c69fe4be4eb305c80184
erazorv4 <joepboons@gmail.com>
2017-03-09 10:10:39 +0100

    Added footer and css and menu and js

---
+++ views/master.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+
+<head>
+    <title>TEST</title>
+    <link href="../css/bootstrap.css" type="text/css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css" />
+    <link href="../css/style.css" type="text/css" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="../js/main.js"></script>
+</head>
+
+<header>
+    <nav class="nav navbar">
+        <div class="container">
+            <div class="topnav hidden-xs hidden-sm">
+                <ul>
+                    <li class=""><a href="/">Neem contact op</a></li>
+                    <li><a class="button" href="http://www.modulairontwerp.local/" role="button">DONEER NU</a></li>
+                </ul>
+            </div>
+            <div class="navbar-header">
+
+                <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-menu, #top-menu">
+                        <span class="bar-wrap">
+                            <span class="bar"></span>
+                            <span class="bar"></span>
+                            <span class="bar"></span>
+                        </span>
+                </button>
+                <!--<a href="/" class="navbar-brand">
+                    <img src="/images/logo.png" alt="Logo" />
+                </a>-->
+            </div>
+            <div class="collapse navbar-collapse" id="main-menu">
+                <ul class="nav navbar-nav">
+
+                    <li class=""><a href="/">Home</a></li>
+                </ul>
+                <!--<div class="topnav visible-xs visible-sm" id="top-menu">
+                    <ul>
+                        <li class=""><a href="/">Home</a></li>
+
+                    </ul>
+                </div>-->
+            </div>
+        </div>
+    </nav>
+</header>
+<body>
+
+<footer>
+    <div class="row">
+        <div class="col-md-4">
+            <h3>About</h3>
+            <p>About text lorum blahablahblah</p>
+        </div>
+        <div class="col-md-4">
+            <h3>Links</h3>
+            <div class="col-md-4">
+                <p><a href="#">Projecten</a></p>
+            </div><div class="col-md-4">
+                <p><a href="#">Over onze Stichting</a></p>
+            </div><div class="col-md-4">
+                <p><a href="#">Doneren</a></p>
+            </div>
+        </div>
+    <div class="col-md-4">
+        <h3>Neem contact op!</h3>
+        <p>Vragen? Mail naar<br />testemailjoep@gmail.com</p>
+    </div>
+</div>
+</footer>
+</body>

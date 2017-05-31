56cebdb773f575bda86a8cf38d4a639c5fff9afe
MaartenVanAlebeek <malebeek@avans.nl>
2017-05-31 14:12:21 +0200

    no message

---
+++ database/seeds/CategoryTableSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->delete();
+
+        $categories = array(
+            array(
+                'id' => 1,
+                'name' => 'Brood',
+                'parent_id' => null,
+                'visible' => 1
+            ),
+            array(
+                'id' => 2,
+                'name' => 'Gebak',
+                'parent_id' => null,
+                'visible' => 1
+            ),
+            array(
+                'id' => 3,
+                'name' => 'Taart',
+                'parent_id' => 2,
+                'visible' => 1
+            ),
+            array(
+                'id' => 4,
+                'name' => 'Bruin',
+                'parent_id' => 1,
+                'visible' => 1
+            ),
+            array(
+                'id' => 5,
+                'name' => 'Wit',
+                'parent_id' => 1,
+                'visible' => 1
+            ),
+        );
+
+        DB::table('categories')->insert($categories);
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
          $this->call('UsersTableSeeder');
          $this->call('NavigationTableSeeder');
          $this->call('ProductSeeder');
+         $this->call('CategoryTableSeeder');
     }
 }

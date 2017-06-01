7244f860bcb437ed8b76fa7c77758e3a46debcb5
erazorv4 <joepboons@gmail.com>
2017-06-01 09:12:48 +0200

    Product Pagina

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,60 +1,44 @@
 <?php
 use App\ShopItemNames;
+
 ?>
 <html>
 
 <head>
-	<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/app.css') }}">
-	<script src="{{ URL::asset('js/app.js') }}"></script>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/app.css') }}">
+    <script src="{{ URL::asset('js/app.js') }}"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-	<body>
-		@include('layouts.header', array('title'=>'product'))
+<body>
+@include('layouts.header', array('title'=>'product'))
 
+<div class="container">
+    <div class="row">
 
-		<div class="container">
-			@php
-			
-			echo "<div class=\"row\">";
-			
-			if ($Id !== null) {		
-									
-				$product = App\Product::find($Id);
-				$name = $product->name;
-				$price = $product->price;
-				$description = $product->description;
-				
-				echo "<h3>$name</h3>";
+        @if ($Id !== null)
+            @php
+                $product = App\Product::find($Id);
+            @endphp
 
-				echo "<div class=\"col-lg-6 col-md-6 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1\" >";
-				
-				echo "<img src=\"../img/WebshopImages/Shop$Id.jpg\" style=\"width: 50%;\">";
-				
-				echo "</div>";
+            <h3>{{$product->name}}</h3>
 
-				echo "<div class=\"col-lg-6 col-md-6 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1\" >";
-				
-				echo "<br> <p>$price</p> <br>";
-				
-				echo "<p>$description</p>";
-				
-				echo "</div>";
-				
-			} else {
-				echo "<h3>Product niet gevonden</h3>";
-			}
-			
-			echo "</div>";
+            <div class="col-lg-6 col-md-6 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1">
+                <img src="../img/WebshopImages/Shop{{$Id}}.jpg" style="width: 50%;">
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-sm-offset-0 col-xs-10 col-xs-offset-1">
+                <br>
+                <p>€{{$product->price}}</p> <br>
+                <p>{{$product->description}}</p>
+            </div>
+        @else
+            <h3>Product niet gevonden</h3>
+        @endif
 
-			echo "</div>";
-			
-			@endphp
-		
-		</div>
-		@include('layouts.footer')
-	</body>
-
+    </div>
+</div>
+@include('layouts.footer')
+</body>
 </html>
